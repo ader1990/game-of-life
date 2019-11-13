d2e6f857a53e4de895aa0e437663ba57a1162b13
Adrian Vladu <avladu@cloudbasesolutions.com>
2019-11-13 13:39:50 +0000

    Moved js code in proper place

--- www/index.js
+++ www/index.js
@@ -1,85 +1,63 @@
 import {
+    memory
+} from "game-of-life/game_of_life_bg";
+
+import {
     Universe,
     Cell
 } from "game-of-life";
-// Import the WebAssembly memory at the top of the file.
-import {
-    memory
-} from "game-of-life/game_of_life_bg";
 
 const CELL_SIZE = 5; // px
 const GRID_COLOR = "#00F";
 const DEAD_COLOR = "#300";
 const ALIVE_COLOR = "#FFF";
 
-let animationId = null;
-let stageBefore0 = 'one';
-let stageBefore1 = '1one';
+const canvas = document.getElementById("game-of-life-canvas");
+const playPauseButton = document.getElementById("play-pause");
+const resetButton = document.getElementById("reset");
+const exploreButton = document.getElementById("explore");
 
-// Construct the universe, and get its width and height.
 const universe = Universe.new();
 const width = universe.width();
 const height = universe.height();
 
+let animationId = null;
+let stageBefore0 = 'one';
+let stageBefore1 = 'none';
+let cellsPtr = null;
+let cells = null;
+let playUntilPeriodic = false
 
-const fps = new class {
-    constructor() {
-        this.fps = document.getElementById("fps");
-        this.frames = [];
-        this.lastFrameTimeStamp = performance.now();
-    }
-
-    render() {
-        // Convert the delta time since the last frame render into a measure
-        // of frames per second.
-        const now = performance.now();
-        const delta = now - this.lastFrameTimeStamp;
-        this.lastFrameTimeStamp = now;
-        const fps = 1 / delta * 1000;
-
-        // Save only the latest 100 timings.
-        this.frames.push(fps);
-        if (this.frames.length > 100) {
-            this.frames.shift();
-        }
-
-        // Find the max, min, and mean of our 100 latest timings.
-        let min = Infinity;
-        let max = -Infinity;
-        let sum = 0;
-        for (let i = 0; i < this.frames.length; i++) {
-            sum += this.frames[i];
-            min = Math.min(this.frames[i], min);
-            max = Math.max(this.frames[i], max);
-        }
-        let mean = sum / this.frames.length;
-
-        // Render the statistics.
-        this.fps.textContent = `
-Frames per Second:
-         latest = ${Math.round(fps)}
-avg of last 100 = ${Math.round(mean)}
-min of last 100 = ${Math.round(min)}
-max of last 100 = ${Math.round(max)}
-`.trim();
-    }
-};
-
-// Give the canvas room for all of our cells and a 1px border
-// around each of them.
-const canvas = document.getElementById("game-of-life-canvas");
 canvas.height = (CELL_SIZE + 1) * height + 1;
 canvas.width = (CELL_SIZE + 1) * width + 1;
 
 const ctx = canvas.getContext('2d');
 
+// lListeners
+playPauseButton.addEventListener("click", event => {
+    if (isPaused()) {
+        play();
+    } else {
+        pause();
+    }
+});
+resetButton.addEventListener("click", event => {
+    playUntilPeriodic = false
+    universe.reset()
+    play()
+})
+
+exploreButton.addEventListener("click", event => {
+    playUntilPeriodic = true
+    universe.reset()
+    play()
+})
+
+
+// Game controls
 const isPaused = () => {
     return animationId === null;
 };
-
-var cellsPtr = null;
-var cells = null;
-var playUntilPeriodic = false
 
 const renderLoop = () => {
     drawGrid();
@@ -104,10 +82,6 @@ const renderLoop = () => {
     animationId = requestAnimationFrame(renderLoop);
 };
 
-const playPauseButton = document.getElementById("play-pause");
-const resetButton = document.getElementById("reset");
-const exploreButton = document.getElementById("explore");
-
 const play = () => {
     playPauseButton.textContent = "stop";
     renderLoop();
@@ -119,26 +93,11 @@ const pause = () => {
     animationId = null;
 };
 
-playPauseButton.addEventListener("click", event => {
-    if (isPaused()) {
-        play();
-    } else {
-        pause();
-    }
-});
-resetButton.addEventListener("click", event => {
-    playUntilPeriodic = false
-    universe.reset()
-    play()
-})
+const getIndex = (row, column) => {
+    return row * width + column;
+};
 
-exploreButton.addEventListener("click", event => {
-    playUntilPeriodic = true
-    universe.reset()
-    play()
-})
-
-
+// Drawing
 const drawGrid = () => {
     ctx.beginPath();
     ctx.strokeStyle = GRID_COLOR;
@@ -156,10 +115,6 @@ const drawGrid = () => {
     }
 
     ctx.stroke();
-};
-
-const getIndex = (row, column) => {
-    return row * width + column;
 };
 
 const drawCells = () => {
@@ -207,4 +162,5 @@ const drawCells = () => {
     return false
 };
 
+// MAIN
 play()
